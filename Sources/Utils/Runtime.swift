80e945405d13c9b755aaffaab4b4c8f4e675d9c8
Max <sibo_jian_29903549@163.com>
2023-10-25 16:34:19 +0800

    feat: Add Runtime.swift to handle object associated value

---
+++ Sources/Utils/Runtime.swift
@@ -0,0 +1,20 @@
+//
+//  Runtime.swift
+//  SBDrawer
+//
+//  Created by JONO-Jsb on 2023/10/25.
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+func getAssociatedObject<T>(_ object: Any, _ key: UnsafeRawPointer) -> T? {
+    return objc_getAssociatedObject(object, key) as? T
+}
+
+func setAssociatedObject<T>(_ object: Any, _ key: UnsafeRawPointer, _ value: T) {
+    objc_setAssociatedObject(object, key, value, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+}
+
+#endif
